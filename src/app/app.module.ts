cd7e32a3b4944891ebaba960f99ed28d8b66cc1b
NETCHECKDOM\jean.cantoral <jean.cantoral@NETCheck.es>
2024-01-17 01:24:07 +0100

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
@@ -16,8 +14,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
