bb6fa16c43f98d837874552a811a74a2daabc423
NETCHECKDOM\jean.cantoral <jean.cantoral@NETCheck.es>
2024-01-17 11:13:32 +0100

    añadido sonido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Howl } from 'howler'; 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'paulaExamen';
+  title = 'PaulaExamen';
+  private backgroundMusic = new Howl({
+    src: ['assets/sonido/Guitarricadelafuente - El Conticinio (Audio).mp3'], // Ruta a tu archivo de audio
+    loop: true, // Reproducción en bucle
+  });
+
+  ngOnInit() {
+    this.backgroundMusic.play(); // Inicia la reproducción al inicializar el componente
+  }
 }
